d1c99004b49dbbe97a5b1f219fdbacbe55ee697a
Syed Ali Mehdi <mehdi55shah@gmail.com>
2023-06-01 15:45:22 +0500

    dependency injection success

--- src/Service.ts
+++ src/Service.ts
@@ -1,11 +1,12 @@
 import { container } from "./main";
 
-export function Service<T extends {new(...args:any[]):{}}>(constructor: T) {
+export const Service = <T extends {new(...args:any[]):{}}>(dependencies?: T[]) => <T extends {new(...args:any[]):{}}>(constructor: T) => {
     if(!container.classes[constructor.name]) {
         container.classes[constructor.name] = constructor;
-        const dependencies = container.dependencies[constructor.name];
-        console.log(container.classes)
-        container.instances[constructor.name] = new constructor("sfas");
+        if(dependencies?.length) {
+            container.instances[constructor.name] = new constructor(...dependencies.map(dependency => new dependency()));
+        }
+        else container.instances[constructor.name] = new constructor();
     }
     return class extends constructor {}
 }
--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,15 @@
 import { Autowired } from "./Autowired";
 import { Service } from "./Service";
+import { container } from "./main";
 
-@Service
+@Service()
 class UserService {
     public async getUser() {
         return { id: 1, name: "Mehdi" }
     }
 }
 
-@Service
+@Service([UserService])
 class Test {
     constructor(
         private userService: UserService
@@ -18,5 +19,8 @@ class Test {
         return await this.userService.getUser()
     }
 }
+
+const test: Test = container.instances['Test']
+test.getUser().then(data => console.log(data))
 
 export default Test
