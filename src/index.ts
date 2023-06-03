fea5c1db0e3bc24e4d440ac26f888d8e986dacad
Syed Ali Mehdi <mehdi55shah@gmail.com>
2023-06-03 19:15:37 +0500

    index file updated

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
 import { container } from "./main";
+import Autowired from './Autowired';
+import Controller from './Controller';
+import Component from './Component';
+import { Service } from './Service';
+import Get from './Get';
+import Post from './Post';
+import Put from './Put';
+import Patch from './Patch';
+import Delete from './Delete';
 
 const express = () => container.app;
 
 export {
     container,
-    express
+    express,
+    Autowired,
+    Controller,
+    Component,
+    Service,
+    Get,
+    Post,
+    Put,
+    Patch,
+    Delete,
 }
